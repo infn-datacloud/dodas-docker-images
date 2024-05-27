63af387f9beff39c804f24a0f3cff4fb9c5297fb
Francesco Sinisi <francesco.sinisi@cnaf.infn.it>
2024-05-27 18:53:57 +0200

    typo

--- docker/spark/spawn.sh
+++ docker/spark/spawn.sh
@@ -33,6 +33,6 @@ else
 
 	cd /.init/
 
-	.init/sts-wire https://iam.cloud.infn.it/ "${USERNAME}" https://rgw.cloud.infn.it/ IAMaccess object "/${USERNAME}" "s3/${USERNAME}" > ".mount_log_${USERNAME}.txt" &
-	.init/sts-wire https://iam.cloud.infn.it/ scratch https://rgw.cloud.infn.it/ IAMaccess object /scratch s3/scratch > .mount_log_scratch.txt &
+	.init/sts-wire https://iam.cloud.infn.it/ "${USERNAME}" https://rgw.cloud.infn.it/ IAMaccess object "/${USERNAME}" "/s3/${USERNAME}" > ".mount_log_${USERNAME}.txt" &
+	.init/sts-wire https://iam.cloud.infn.it/ scratch https://rgw.cloud.infn.it/ IAMaccess object /scratch /s3/scratch > .mount_log_scratch.txt &
 fi
